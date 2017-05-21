039e7ecfe4177b889ba883c57ff30ec2496d2308
Khakhana Thimachai <khakhana@bluebik.com>
2017-05-21 20:11:33 +0700

    5. stringTemplate

--- src/i_introduction/_5_String_Templates/StringTemplates.kt
+++ src/i_introduction/_5_String_Templates/StringTemplates.kt
@@ -19,7 +19,7 @@ you don't need to escape a backslash by a backslash.
 String template entries (${42}) are allowed here.
 """
 
-fun getPattern() = """\d{2}\.\d{2}\.\d{4}"""
+fun getPattern() = """\d{2} $month \d{4}"""
 
 fun example() = "13.06.1992".matches(getPattern().toRegex()) //true
 
@@ -35,4 +35,6 @@ fun todoTask5(): Nothing = TODO(
     documentation = doc5(),
     references = { getPattern(); month })
 
-fun task5(): String = todoTask5()
+fun task5(): String {
+    return getPattern()
+}
