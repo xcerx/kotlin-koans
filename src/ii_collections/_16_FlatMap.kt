39447a330746919718492fba323a82ddde893b87
Khakhana Thimachai <khakhana@bluebik.com>
2017-05-29 13:53:40 +0700

    16._FlatMap

--- src/ii_collections/_16_FlatMap.kt
+++ src/ii_collections/_16_FlatMap.kt
@@ -8,11 +8,9 @@ fun example() {
 }
 
 val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
+    return this.orders.flatMap { it.products.toList() }.toSet()
 }
 
 val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+    return this.customers.flatMap { it.orderedProducts.toList() }.toSet()
 }
