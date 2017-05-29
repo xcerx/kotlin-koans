c72e5e1835967cccfe850220e9071fe02f648728
Khakhana Thimachai <khakhana@bluebik.com>
2017-05-30 00:12:26 +0700

    26_In_Range

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,7 +2,9 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +12,10 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+
+    operator fun contains(date: MyDate): Boolean {
+        return date >= start && date <= endInclusive
+    }
+
+}
