c774cbd0a5189c7f29075d700c613f7b20fb0e40
Khakhana Thimachai <khakhana@bluebik.com>
2017-05-31 01:55:29 +0700

    32_Properties

--- src/iv_properties/_32_Properties_.kt
+++ src/iv_properties/_32_Properties_.kt
@@ -1,19 +1,12 @@
 package iv_properties
 
-import util.TODO
-import util.doc32
-
 class PropertyExample() {
+    var property = 0
     var counter = 0
-    var propertyWithCounter: Int? = todoTask32()
+    var propertyWithCounter: Int?
+        get() = property
+        set(value) {
+            property = value as Int
+            counter += 1
+        }
 }
-
-fun todoTask32(): Nothing = TODO(
-    """
-        Task 32.
-        Add a custom setter to PropertyExample.propertyWithCounter so that
-        the 'counter' property is incremented every time 'propertyWithCounter' is assigned to.
-    """,
-    documentation = doc32(),
-    references = { PropertyExample() }
-)
