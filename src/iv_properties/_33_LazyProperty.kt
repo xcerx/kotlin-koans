032f4bb44df531f64aefb0e26dceda1b3075591e
Khakhana Thimachai <khakhana@bluebik.com>
2017-05-31 02:02:51 +0700

    3_LazyProperties but i dont under stand

--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -3,7 +3,9 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    val lazy: Int by lazy(initializer) {
+        initializer.invoke()
+    }
 }
 
 fun todoTask33(): Nothing = TODO(
